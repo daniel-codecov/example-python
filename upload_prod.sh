40db5210c806b275d1718a6217d2219e8298a731
Daniel Yu <daniel.yu@sentry.io>
2024-03-18 17:25:57 -0400

    add scripts

--- upload_prod.sh
+++ upload_prod.sh
@@ -2,7 +2,7 @@
 
 codecovcli create-commit -t 663eb710-98ff-4051-b11b-9a43710ca5cf 
 codecovcli create-report -t 663eb710-98ff-4051-b11b-9a43710ca5cf 
-codecovcli do-upload --file coverage/coverage_a -t 663eb710-98ff-4051-b11b-9a43710ca5cf -F a
-codecovcli do-upload --file coverage/coverage_b -t 663eb710-98ff-4051-b11b-9a43710ca5cf -F b
-codecovcli do-upload --file coverage/coverage_c -t 663eb710-98ff-4051-b11b-9a43710ca5cf -F c
+codecovcli do-upload --file coverage/coverage_x.xml -t 663eb710-98ff-4051-b11b-9a43710ca5cf -F x --disable-search
+codecovcli do-upload --file coverage/coverage_y.xml -t 663eb710-98ff-4051-b11b-9a43710ca5cf -F y --disable-search
+codecovcli do-upload --file coverage/coverage_z.xml -t 663eb710-98ff-4051-b11b-9a43710ca5cf -F z --disable-search
 
