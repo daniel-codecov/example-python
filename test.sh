b3ccd4344ce72c1727b162a04a1ec3f63ef00dbf
Daniel Yu <daniel.yu@sentry.io>
2024-03-18 12:54:46 -0400

    another commit

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
-pytest tests/test_calculator.py --cov --cov-report=xml:coverage/coverage_a
-pytest tests/test_expression.py --cov --cov-report=xml:coverage/coverage_b
-pytest tests/test_expression_parser.py --cov --cov-report=xml:coverage/coverage_c
+pytest tests/test_calculator.py --cov --cov-report=xml:coverage/coverage_a.xml --cov-config=.coveragerc
+pytest tests/test_expression.py --cov --cov-report=xml:coverage/coverage_b.xml --cov-config=.coveragerc
+pytest tests/test_expression_parser.py --cov --cov-report=xml:coverage/coverage_c.xml --cov-config=.coveragerc
