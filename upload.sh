3bd8f1df7bdb2b180fb112e92105af2878713aa1
Daniel Yu <daniel.yu@sentry.io>
2024-03-18 13:29:10 -0400

    commit1

--- upload.sh
+++ upload.sh
@@ -3,6 +3,6 @@
 codecovcli-helper create-commit
 codecovcli-helper create-report
 codecovcli-helper do-upload --file coverage/coverage_x.xml -F x --disable-search
-# codecovcli-helper do-upload --file coverage/coverage_y.xml -F y --disable-search
-# codecovcli-helper do-upload --file coverage/coverage_z.xml -F z --disable-search
+codecovcli-helper do-upload --file coverage/coverage_y.xml -F y --disable-search
+codecovcli-helper do-upload --file coverage/coverage_z.xml -F z --disable-search
 
