4dbe63c0de21aecce7c3c7dbd4bc267f65f4b892
Daniel Yu <daniel.yu@sentry.io>
2024-03-18 12:45:09 -0400

    remove tests

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,12 +2,12 @@ import pytest
 from app.calculator import Calculator
 
 
-def test_add():
-    assert Calculator.add(1, 2) == 3.0
-    assert Calculator.add(1.0, 2.0) == 3.0
-    assert Calculator.add(0, 2.0) == 2.0
-    assert Calculator.add(2.0, 0) == 2.0
-    assert Calculator.add(-4, 2.0) == -2.0
+# def test_add():
+#     assert Calculator.add(1, 2) == 3.0
+#     assert Calculator.add(1.0, 2.0) == 3.0
+#     assert Calculator.add(0, 2.0) == 2.0
+#     assert Calculator.add(2.0, 0) == 2.0
+#     assert Calculator.add(-4, 2.0) == -2.0
 
 
 def test_subtract():
