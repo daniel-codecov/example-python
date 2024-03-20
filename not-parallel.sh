bca8deebe2deb4a9b44a547505465e0b68458ed0
Daniel Yu <daniel.yu@sentry.io>
2024-03-20 10:42:55 -0400

    stash

--- not-parallel.sh
+++ not-parallel.sh
@@ -1,10 +1,11 @@
 # /bin/bash
 
+git stash
 git checkout 3bd8f1df7bdb2b180fb112e92105af2878713aa1
 ./upload.sh
 
+git stash
 git checkout 03d509048862234d3dcfcb1c1025053e7506a1a8
-# ./upload_partial.sh
 codecovcli-helper create-commit
 codecovcli-helper create-report
 codecovcli-helper do-upload --file coverage/coverage_x.xml -F x --disable-search
